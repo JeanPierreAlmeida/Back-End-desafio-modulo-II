7dc208c75bffc14c89c81d75bda5c226a5c0d2ba
JeanSisse <tavares1988@live.com>
2021-09-21 00:26:07 -0300

    Adicionando documentaçao com swagger e utilizaçao de eslint com estilo airbnb

--- src/servidor.js
+++ src/servidor.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 const roteador = require('./rotas/roteador');
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(roteador);
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(require('../swagger.json')));
 
 module.exports = app;
